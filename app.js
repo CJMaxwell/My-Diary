37b12446d11edaf7629d8496c124f9dace36c43b
maxwell <chukwunonyeremmaxwell@gmail.com>
2018-07-15 20:41:52 +0100

    Created user API end-points

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import authRoutes from './routes/authRoutes';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
